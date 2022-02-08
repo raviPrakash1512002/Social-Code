86af0025ce2b42e04233206ec655b4719da20151
Ravi <raviprakash1512002@gmail.com>
2022-02-09 01:58:47 +0530

    chnage url of chat server

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine {
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000',{ transports: ['websocket'] });
+        this.socket = io.connect('http://15.206.123.20:5000',{ transports: ['websocket'] });
 
         if (this.userEmail) {
             this.connectionHandler();
